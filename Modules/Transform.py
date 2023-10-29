48c040509c7a975088175620afb8166826e6d1c2
richardlee-kr <richardl22245@gmail.com>
2023-10-29 17:17:46 +0900

    [ADD] Translate and Rotate

--- Modules/Transform.py
+++ Modules/Transform.py
@@ -1,9 +1,22 @@
 from Modules.Component import *
 from Modules.Vector import *
+from math import *
 
 class Transform(Component):
     def __init__(self):
+        self.name = "Transform"
         self.position = Vector.zero
         self.rotation = 0
         self.parent = None
         self.child = list()
+        self.childCount = len(self.child)
+        self.right = Vector.right
+        self.up = Vector.up
+
+    def Translate(self, v):
+        self.position += v
+
+    def Rotate(self, angle):
+        self.rotation += angle % 360
+        self.right = Vector(math.cos(math.radians(angle)),math.sin(math.radians(angle)))
+        self.up = Vector(-1 * math.sin(math.radians(angle)), math.cos(math.radians(angle)))
