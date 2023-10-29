f98561a7e2d40e92047156de2941a6fa8fddac41
richardlee-kr <richardl22245@gmail.com>
2023-10-29 17:18:01 +0900

    [ADD] GetComponent

--- Modules/GameObject.py
+++ Modules/GameObject.py
@@ -1,12 +1,19 @@
 from Modules.Transform import *
 
 class GameObject:
-    def __init__(self):
-        self.name = "_"
-        self.components = list()
+    def __init__(self, name):
+        self.name = name
         self.transform = Transform()
+        self.components = list()
+        self.components.append(self.transform)
     
     def AddComponent(self, cmpnt):
         self.components.append(cmpnt)
         cmpnt.gameObject = self
         cmpnt.transform = self.transform
+
+    def GetComponent(self, cmpnt):
+        for item in self.components:
+            if cmpnt == item.name:
+                print("find")
+                return item
