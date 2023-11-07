3622a2c3fd05ee532bce347da17d21c40862b628
richardlee-kr <richardl22245@gmail.com>
2023-11-07 09:05:24 +0900

    [REFACT] move pygame initailize to __init__

--- UniPy/__init__.py
+++ UniPy/__init__.py
@@ -9,3 +9,17 @@ from .Collision import *
 from .Scene import *
 from .Camera import *
 from .Color import *
+
+import asyncio
+import pygame
+WIDTH = 1080
+HEIGHT = 720
+FPS = 120
+
+pygame.init()
+clock = pygame.time.Clock()
+SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
+
+scene = Scene("Maverick Gunner", SCREEN)
+physics = Physics(scene)
+pygame.display.set_caption(scene.sceneName)
