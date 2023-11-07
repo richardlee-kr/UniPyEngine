4fa828f0317f1812273df9575dafefd51f628c1c
richardlee-kr <richardl22245@gmail.com>
2023-11-07 15:03:04 +0900

    [ADD] Scene.FindObject({name})

--- UniPy/Scene.py
+++ UniPy/Scene.py
@@ -4,11 +4,19 @@ class Scene:
         self.screen = screen
         self.hierarchy = list()
 
+    def FindObject(self, name):
+        for object in self.hierarchy:
+            if object.name == name:
+                return object
+        return None
+
     def Update(self):
         for object in self.hierarchy:
             if object.transform.position.x > 1080 + 20 or object.transform.position.x < 0 - 20 or object.transform.position.y > 720 + 20 or object.transform.position.y < 0 - 20:
                 self.hierarchy.remove(object)
+                continue
 
+            #print(len(self.hierarchy))
             object.Update()
             try:
                 object.GetComponent("SpriteRenderer").Render(self.screen)
