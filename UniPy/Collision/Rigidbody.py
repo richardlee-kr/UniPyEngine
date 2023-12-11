2c32a6752858f44383e1cc1daac37bd421cf6026
richardlee-kr <richardl22245@gmail.com>
2023-12-11 16:41:14 +0900

    [ADD] Rigidbody elements

--- UniPy/Collision/Rigidbody.py
+++ UniPy/Collision/Rigidbody.py
@@ -1,1 +1,19 @@
 # TODO Rigidbody
+from ..Component import *
+from ..Vector import *
+
+class Rigidbody(Component):
+    def __init__(self):
+        super(Rigidbody, self).__init__()
+        self.name = "Rigidbody"
+
+        self.mass = 1
+        self.drag = 0
+        self.angularDrag = 0.05
+        self.useGravity = True
+
+        self.angularVelocity = Vector.zero
+        self.velocity = Vector.zero
+
+    def AddForce(self, force:Vector):
+        pass
