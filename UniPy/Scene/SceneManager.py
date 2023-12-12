e61b364f7e6fedf75dce72dc3885a19e630dde85
richardlee-kr <richardl22245@gmail.com>
2023-12-12 20:21:06 +0900

    [ADD] scene index clipping

--- UniPy/Scene/SceneManager.py
+++ UniPy/Scene/SceneManager.py
@@ -5,9 +5,19 @@ class SceneManager:
     def __init__(self):
         self.sceneList = list()
         self.currentScene:Scene = None
+        self.index = 0
 
     def AddScene(self, scene:Scene):
         self.sceneList.append(scene)
+        if self.currentScene == None:
+            self.currentScene = self.sceneList[0]
+            self.index = 0
 
     def LoadScene(self, index:int):
+        if index < 0:
+            index = 0
+        if index > len(self.sceneList):
+            index = len(self.sceneList)
         self.currentScene = self.sceneList[index]
+        self.index = index
+        pygame.display.set_caption(self.currentScene.sceneName)
